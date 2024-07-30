24e198a203937f41dad23e89727338b6eb9b6764
Capita1 <145567743+Capita1@users.noreply.github.com>
2024-07-30 20:15:41 -0300

    Add files via upload

---
+++ nvim/init.lua
@@ -0,0 +1,7 @@
+vim.g.neovide_scale_factor = 1
+vim.g.neovide_transparency = 0.7
+vim.g.neovide_background_color = "#240f16"
+
+vim.wo.number = true  
+vim.wo.wrap = false
+vim.o.guifont = "Terminess Nerd Font Mono:h15:#e-antialias:b"
