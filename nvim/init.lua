01cf83d5449d1ea62f07757b54ad1e86bbdec39c
Capita1 <capita1_gabriel@outlook.com>
2024-10-29 11:47:53 -0300

    Nvim usavel tinha muito plugins no LazyVim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,14 @@
-vim.g.neovide_scale_factor = 1
-vim.g.neovide_transparency = 0.7
-vim.g.neovide_background_color = "#240f16"
-
+--NVIM config
 vim.wo.number = true  
 vim.wo.wrap = false
 vim.opt.relativenumber = true
 vim.opt.smartindent = true
 vim.o.guifont = "Terminess Nerd Font Mono:h15:#e-antialias:b"
--- bootstrap lazy.nvim, LazyVim and your plugins
+--NEOVIDE config
+vim.g.neovide_scale_factor = 1
+vim.g.neovide_transparency = 0.5
+vim.g.neovide_background_color = "#000000"
+--LAZY plugins
 require("config.lazy")
+--LSP server
+require'lspconfig'.pyright.setup{}
