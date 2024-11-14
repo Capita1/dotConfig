9185f3a1705a427781683427630b6fe9ead4dfd8
Capita1 <capita1_gabriel@outlook.com>
2024-11-14 14:31:00 -0300

    Damn

---
+++ README.md
@@ -0,0 +1,26 @@
+# .config
+
+![GitHub repo size](https://img.shields.io/github/repo-size/iuricode/README-template?style=for-the-badge)
+![GitHub language count](https://img.shields.io/github/languages/count/iuricode/README-template?style=for-the-badge)
+![GitHub forks](https://img.shields.io/github/forks/iuricode/README-template?style=for-the-badge)
+![Bitbucket open issues](https://img.shields.io/bitbucket/issues/iuricode/README-template?style=for-the-badge)
+![Bitbucket open pull requests](https://img.shields.io/bitbucket/pr-raw/iuricode/README-template?style=for-the-badge)
+
+
+## 🤝 Colaboradores
+
+Agradecemos às seguintes pessoas que contribuíram para este projeto:
+
+<table>
+  <tr>
+    <td align="center">
+      <a href="#" title="defina o título do link">
+        <img src="https://avatars.githubusercontent.com/u/145567743?v=4" width="100px;" alt="Foto minha"/><br>
+        <sub>
+          <b>Gabriel Henrique Pereira Leite</b>
+        </sub>
+      </a>
+    </td>
+
+  </tr>
+</table>
---
+++ apps.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+echo "Basico"
+sudo pacman -Syu #atualizar
+sudo pacman -S --needed git base-devel flatpak
+
+#BAIXAR E INSTALAR PARU
+if ! [ -x "$(command -v paru)" ]; then
+	echo -e 'paru não instalado\nInstalando...' >&2
+	git clone https://aur.archlinux.org/paru.git 
+	cd paru
+	makepkg -si
+else
+	echo 'paru já instalado.' >&2
+fi
+
+
+echo "Interface"			
+sudo pacman -S --needed hyprland swaync waybar wofi wpaperd
+echo "Apps"
+sudo pacman -S --needed kitty pavucontrol blueman thunar thunar-media-tags-plugin thunar-shares-plugin thunar-volman ffmpegthumbnailer tumbler
+echo "Texto"			
+sudo pacman -S --needed libreoffice-still neovide neovim mousepad zathura zathura-pdf-mupdf 
+echo "Midia"
+sudo pacman -S --needed feh vlc mpv playerctl
+echo "CLI apps"
+sudo pacman -S --needed neofetch btop awk less libnotify yt-dlp ffmpeg cliphist wl-clipboard
+echo "AUR apps"
+paru -S librewolf-bin #navegador
+paru -S waydroid #android container
+
+flatpak install org.ferdium.Ferdium #client de apps de mensagem
+flatpak install org.onionshare.OnionShare #serviços onion
+flatpak install	com.stremio.Stremio #stremio
+flatpak install	io.github.seadve.Mousai #reconhecedor de musica
+flatpak install io.gitlab.adhami3310.Converter #conversor de imagens
+flatpak install fr.handbrake.ghb #conversor de videos
