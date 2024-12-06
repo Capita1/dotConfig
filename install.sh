7516445680e1844a95d012f8232d1db9b71e0b44
Capita1 <gabrielhenrik25@gmail.com>
2024-12-06 10:38:32 -0300

    CORES

--- install.sh
+++ install.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
-echo "Basico"
-sudo pacman -Syu #atualizar
-sudo pacman -S --needed base-devel flatpak github-cli
-
+BLU='\033[0;34m'
+RED='\033[0;31m'
+YEL='\033[0;33m'
+NC='\033[0m' 
 if [ "$(pwd)" = "/home/$(whoami)/.config/dotConfig" ]; then
-	echo "Links"	
-	cp -r config/hypr/ ../
-	ln -rsf config/* ../
-	ln -rsf config/hypr/temas/Simples/* ~/.config/
+	echo -e "${BLU}\n░░░░█████╗░░█████╗░███╗░░██╗███████╗██╗░██████╗░"
+	echo -e "${BLU}░░░██╔══██╗██╔══██╗████╗░██║██╔════╝██║██╔════╝░"
+	echo -e "${BlU}░░░██║░░╚═╝██║░░██║██╔██╗██║█████╗░░██║██║░░██╗░"
+	echo -e "${BLU}░░░██║░░██╗██║░░██║██║╚████║██╔══╝░░██║██║░░╚██╗"
+	echo -e "${BLU}██╗╚█████╔╝╚█████╔╝██║░╚███║██║░░░░░██║╚██████╔╝"
+	echo -e "${BLU}╚═╝░╚════╝░░╚════╝░╚═╝░░╚══╝╚═╝░░░░░╚═╝░╚═════╝░"
 else
-	cd ~/.config/dotConfig 
+	cd ~/.config/
+	git clone https://github.com/Capita1/dotConfig.git
+	cd dotConfig/
+	sh install.sh
 fi
 
-echo "Configs"	
+echo -e "${YEL}\nAtualizando...\n${NC}"
+sudo pacman -Syu --needed base-devel 
+
+echo "${YEL}Links${NC}"	
+cp -r config/hypr/ ../
+ln -rsf config/* ../
+ln -rsf config/hypr/temas/Simples/* ~/.config/
 sudo rm ~/.bashrc ; sudo ln -rsf bashrc ~/.bashrc
 sudo rm ~/.profile ; sudo ln -rsf profile ~/.profile
 sudo rm /etc/hosts ; sudo ln -rsf hosts /etc/
 
-######
-#APPS#
-######
 
-echo "Interface"			
+echo -e "${YEL}\nApps\n${NC}"
+#Interface
 sudo pacman -S --needed hyprland swaync waybar wofi wpaperd nwg-look polkit-kde-agent xdg-desktop-portal-gtk xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-hyprland
-echo "Apps"
+#Apps
 sudo pacman -S --needed kitty pavucontrol blueman thunar thunar-media-tags-plugin thunar-shares-plugin thunar-volman ffmpegthumbnailer tumbler gvfs gparted
-echo "Texto"
+#Texto
 sudo pacman -S --needed libreoffice-still neovide neovim mousepad zathura zathura-pdf-mupdf 
-echo "Midia"
+#Midia
 sudo pacman -S --needed feh vlc mpv playerctl
-echo "CLI apps"
-sudo pacman -S --needed neofetch btop awk less libnotify yt-dlp ffmpeg cliphist wl-clipboard tealdeer unzip
-echo "Fontes"
+#CLI
+sudo pacman -S --needed neofetch btop awk less libnotify yt-dlp ffmpeg cliphist wl-clipboard tealdeer unzip github-cli flatpak
+#Fontes
 sudo pacman -S --needed ttf-nerd-fonts-symbols-mono ttf-terminus-nerd adobe-source-code-pro-fonts
